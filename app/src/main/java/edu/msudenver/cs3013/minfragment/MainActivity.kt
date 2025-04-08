7b9343343a8493588397b4e87d9ea5e92c483282
konkelmp <preston.konkel@gmail.com>
2025-04-08 15:21:50 -0600

    Initial commit

---
+++ app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
@@ -0,0 +1,29 @@
+package edu.msudenver.cs3013.minfragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentContainerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun startFragment(v: View) {
+        findViewById<FragmentContainerView>(
+            R.id.blue_fragment)?.let {
+                framelayout ->
+                val blueFragment = BlueFragment.newInstance("FIRST")
+            supportFragmentManager.beginTransaction().replace(framelayout.id, blueFragment).addToBackStack(null).commit()
+        }
+    }
+}
